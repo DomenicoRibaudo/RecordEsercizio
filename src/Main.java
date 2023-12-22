83d04360416eaf80a43dc21eb23b200ce620cb74
Domenico <domenicoribaudo06@gmail.com>
2023-12-22 23:21:57 +0100

    Commit finale, esercizio compleatato

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//Definisci un oggetto Record che rappresenti un Punto con le coordinate x & y
+
+public class Main {
+    public static void main(String[] args) {
+        Punto coordinate = new Punto(3.4, 4.6);
+        System.out.println(coordinate);
+    }
+}
